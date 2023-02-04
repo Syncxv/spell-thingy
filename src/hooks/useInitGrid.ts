82a7fabeb5c9baef976b5576ee4c5dd20f59f5e2
Syncxv <aruldevarackal1@gmail.com>
2023-02-05 02:16:56 +1100

    YOOOOOOOOO

--- src/hooks/useInitGrid.ts
+++ src/hooks/useInitGrid.ts
@@ -1,8 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 
 import { Letter } from "../types";
-import { directions } from "../utils/constants";
 import { getValue } from "../utils/getValue";
+import { uuidv4 } from "../utils/uuidv4";
 
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -15,7 +15,7 @@ export const useInitGrid = (size = 5) => {
             tempGrid[row] = [];
             for (let col = 0; col < size; col++) {
                 const randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
-                tempGrid[row][col] = { key: randomLetter, value: getValue(randomLetter), row, column: col };
+                tempGrid[row][col] = { id: uuidv4(), key: randomLetter, value: getValue(randomLetter), row, column: col };
             }
         }
 
@@ -43,32 +43,26 @@ export const useInitGrid = (size = 5) => {
                 grid.flat().forEach(m => m.ref?.classList.remove("selected"));
                 selectedLetters.current = [];
             }
-            if (letter.ref && letter.ref.classList.contains("selected")) {
-                const currentLetterIndex = selectedLetterz.findIndex(e => e === letter);
-                selectedLetterz.splice(currentLetterIndex, 1);
-                letter.ref.classList.remove("selected");
-            } else {
+            for (var selectionType = -1, o = 0; o < selectedLetterz.length; o++)
+                if (selectedLetterz[o].id === letter.id) {
+                    selectionType = o;
+                    break;
+                }
+            console.log(selectionType);
+            // -1 means select letter XD
+            if (selectionType === -1) {
                 letter.ref?.classList.add("selected");
                 selectedLetterz.push(letter);
-            }
-        },
-
-
-        getNeighbours(letter: Letter) {
-            const neighbors: Letter[] = [];
-            for (const [dx, dy] of directions) {
-                const x2 = letter.row + dx;
-                const y2 = letter.column + dy;
-
-                if (x2 >= 0 && x2 < grid.length && y2 >= 0 && y2 < grid[0].length) {
-                    neighbors.push(grid[x2][y2]);
-                }
 
             }
-            return neighbors;
-
+            // deselect layer XD
+            else if (selectionType === selectedLetterz.length - 2) {
+                console.log(":O");
+                var letterToDeSelect = selectedLetterz[selectedLetterz.length - 1];
+                selectedLetters.current.pop();
+                letterToDeSelect?.ref?.classList.remove("selected");
+            }
         },
-
 
         isAdjecent(a: Letter, b: Letter) {
             return Math.abs(a.column - b.column) <= 1 && Math.abs(a.row - b.row) <= 1;
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,6 @@
 export type Letter = {
+    // uuidv4
+    id: string
     // the char code - 64. A = 1, B = 2
     value: number
     // A, B, C etc yk
