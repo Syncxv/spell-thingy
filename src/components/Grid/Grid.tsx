0a7f8b0f96dcc19d51e5271f8e36d9a017e52571
Syncxv <aruldevarackal1@gmail.com>
2023-02-04 22:36:47 +1100

    gon try mouseover this time
    and eslint not working ill fix in a bit

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -1,20 +1,49 @@
+import { MouseEvent, useRef } from 'react'
 import { useInitGrid } from '../../hooks/useInitGrid'
+import { getLetterByRowCol } from '../../utils/getLetterByRowCol'
+
+export const SIZE = 5
+
 export const Grid: React.FC<{}> = () => {
-	const size = 5
-	const grid = useInitGrid(size)
-	console.log(grid)
+	const grid = useInitGrid(SIZE)
+	const isMouseDownRef = useRef(false)
+
+	const handleMouseDown = (e: MouseEvent<HTMLDivElement>) => {
+		isMouseDownRef.current = true
+		handleMouseOver(e)
+	}
+
+	const handleMouseUp = () => {
+		isMouseDownRef.current = false
+	}
+
+	const handleMouseOver = (e: MouseEvent<HTMLDivElement>) => {
+		if (!isMouseDownRef.current) return
+		const target = e.target as HTMLDivElement
+		const letterKeys = target.dataset.letter
+		if (!letterKeys) return
+
+		const [row, col] = letterKeys.split(',')
+		const letter = getLetterByRowCol(grid, parseInt(row), parseInt(col))
+
+		console.log(letter)
+	}
 	return (
 		<>
 			<div
-				style={{ gridTemplateColumns: `repeat(${size}, 1fr)` }}
+				style={{ gridTemplateColumns: `repeat(${SIZE}, 1fr)` }}
 				className="grid-wrapper grid gap-4 w-[80vw] h-[80vh]"
 			>
-				{grid.flat().map((cell, i) => (
+				{grid.flat().map((letter, i) => (
 					<div
-						key={i}
-						className="flex items-center justify-center text-center text-3xl bg-slate-200 p-8 text-slate-900 rounded-md"
+						key={`${letter.row},${letter.column}`}
+						className="flex items-center justify-center text-center text-3xl bg-slate-200 p-8 text-slate-900 rounded-md select-none"
+						onMouseDown={handleMouseDown}
+						onMouseUp={handleMouseUp}
+						onMouseOver={(e) => handleMouseOver(e)}
+						data-letter={`${letter.row},${letter.column}`}
 					>
-						{cell.key}
+						{letter.key}
 					</div>
 				))}
 			</div>
---
+++ src/utils/getLetterByRowCol.ts
@@ -0,0 +1,3 @@
+import { Letter } from '../types'
+export const getLetterByRowCol = (grid: Letter[][], row: number, col: number) =>
+	grid.flat().find((l) => l.row === row && l.column === col)
