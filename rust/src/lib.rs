3caf9984ed896af2f16d4a5ba0e550d0385ebfa4
Syncxv <aruldevarackal1@gmail.com>
2023-02-06 06:53:04 +1100

    IT WORKS?

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,7 +1,8 @@
 use rand::Rng;
-use std::time::SystemTime;
+// use std::time::SystemTime;
 
 use serde::{Deserialize, Serialize};
+use serde_json;
 use wasm_bindgen::prelude::*;
 
 use web_sys::console;
@@ -13,14 +14,14 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[wasm_bindgen]
-struct Letter {
+pub struct Letter {
     id: String,
     // A, B, C etc yk
-    key: char,
+    pub key: char,
 
-    row: i32,
+    pub row: i32,
 
-    column: i32,
+    pub column: i32,
 }
 
 impl Letter {
@@ -48,7 +49,7 @@ const DIRECTIONS: [[i32; 2]; 8] = [
 ];
 
 fn get_all_combinations<'a>(
-    grid: &'a Vec<Vec<Letter>>,
+    grid: &'a Vec<Letter>,
     row: i32,
     col: i32,
     visited: &mut Vec<Vec<bool>>,
@@ -62,7 +63,7 @@ fn get_all_combinations<'a>(
 
     visited[row as usize][col as usize] = true;
 
-    combination.push(&grid[row as usize][col as usize]);
+    combination.push(&grid[row as usize * col as usize + col as usize]);
     if combination.len() == desired as usize {
         all_combinations.push(combination);
     } else {
@@ -91,9 +92,9 @@ fn get_random_char() -> char {
 }
 
 #[wasm_bindgen]
-pub fn getWords(_grid: JsValue) {
+pub fn getWords(_grid: JsValue, n: i32) -> String {
     console::log_1(&"bruh1".into());
-    let grid = jsvalue_to_2d_array(_grid).unwrap();
+    let grid: Vec<Letter> = _grid.into_serde().unwrap();
 
     console::log_1(&"bruh2".into());
 
@@ -102,7 +103,7 @@ pub fn getWords(_grid: JsValue) {
     let mut visited: Vec<Vec<bool>> = vec![vec![false; SIZE as usize]; SIZE as usize];
 
     // start the recursive process from each cell in the grid
-    let start = SystemTime::now();
+    // let start = SystemTime::now();
 
     for row1 in 0..SIZE {
         for col1 in 0..SIZE {
@@ -113,12 +114,12 @@ pub fn getWords(_grid: JsValue) {
                 &mut visited,
                 combination.clone(),
                 &mut all_combinations,
-                10,
+                n,
             );
         }
     }
-    let end = SystemTime::now();
-    let duration = end.duration_since(start).unwrap();
+    // let end = SystemTime::now();
+    // let duration = end.duration_since(start).unwrap();
 
     // for (_, row) in all_combinations.iter().enumerate() {
     //     let res = row
@@ -128,10 +129,10 @@ pub fn getWords(_grid: JsValue) {
     //         .join("");
     //     print!("{}\n", res);
     // }
-    println!("it took {} seconds", duration.as_secs());
+    // println!("it took {} seconds", duration.as_secs());
     println!("ho, {:?}", all_combinations.len());
-}
+    console::log_1(&"bruh3 done".into());
 
-fn jsvalue_to_2d_array(js_value: JsValue) -> Option<Vec<Vec<Letter>>> {
-    js_value.into_serde().ok()
+    let serialized = serde_json::to_string(&all_combinations).unwrap();
+    serialized
 }
