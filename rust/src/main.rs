1d9c83f6c8b6497270e975662ad4bbdee81cbc3c
Syncxv <aruldevarackal1@gmail.com>
2023-02-05 15:19:32 +1100

    handle most warning
    but code still garrbage

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,12 +1,11 @@
 use std::time::SystemTime;
 
-use rand;
+use rand::Rng;
+
 use uuid::Uuid;
 #[derive(Debug, Clone)]
 struct Letter {
     id: Uuid,
-    // the char code - 64. A = 1, B = 2
-    value: i32,
     // A, B, C etc yk
     key: char,
 
@@ -16,10 +15,9 @@ struct Letter {
 }
 
 impl Letter {
-    pub fn new(id: Uuid, value: i32, key: char, row: i32, column: i32) -> Letter {
+    pub fn new(id: Uuid, key: char, row: i32, column: i32) -> Letter {
         Letter {
             id,
-            value,
             column,
             key,
             row,
@@ -27,8 +25,8 @@ impl Letter {
     }
 }
 
-const alphabet: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const size: i32 = 5;
+const ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const SIZE: i32 = 5;
 const DIRECTIONS: [[i32; 2]; 8] = [
     [-1, 0],  // up
     [0, 1],   // right
@@ -40,33 +38,33 @@ const DIRECTIONS: [[i32; 2]; 8] = [
     [-1, -1], // up-left
 ];
 
-fn getAllCombinations<'a>(
+fn get_all_combinations<'a>(
     grid: &'a Vec<Vec<Letter>>,
     row: i32,
     col: i32,
     visited: &mut Vec<Vec<bool>>,
     mut combination: Vec<&'a Letter>,
-    allCombinations: &mut Vec<Vec<&'a Letter>>,
+    all_combinations: &mut Vec<Vec<&'a Letter>>,
     desired: i32,
 ) {
-    if row < 0 || row >= size || col < 0 || col >= size || visited[row as usize][col as usize] {
+    if row < 0 || row >= SIZE || col < 0 || col >= SIZE || visited[row as usize][col as usize] {
         return;
     }
 
     visited[row as usize][col as usize] = true;
 
     combination.push(&grid[row as usize][col as usize]);
-    if (combination.len() == desired as usize) {
-        allCombinations.push(combination);
+    if combination.len() == desired as usize {
+        all_combinations.push(combination);
     } else {
         for [dx, dy] in DIRECTIONS.into_iter() {
-            getAllCombinations(
+            get_all_combinations(
                 grid,
                 row + dx,
                 col + dy,
                 visited,
                 combination.clone(),
-                allCombinations,
+                all_combinations,
                 desired,
             );
         }
@@ -75,37 +73,43 @@ fn getAllCombinations<'a>(
     // combination.clone().pop();
     visited[row as usize][col as usize] = false;
 }
+
+fn get_random_char() -> char {
+    let mut rng = rand::thread_rng();
+    let random_index = rng.gen_range(0, ALPHABET.len());
+    let random_char = ALPHABET.chars().nth(random_index).unwrap();
+    random_char
+}
 fn main() {
     let mut grid: Vec<Vec<Letter>> = vec![];
-    for row in 0..size {
+    for row in 0..SIZE {
         grid.push(vec![]);
-        for col in 0..size {
+        for col in 0..SIZE {
             grid[row as usize].push(Letter::new(
                 Uuid::new_v4(),
-                0,
-                rand::sample(&mut rand::thread_rng(), alphabet.chars(), 1)[0],
+                get_random_char(),
                 row.try_into().unwrap(),
                 col,
             ));
         }
     }
 
-    let mut allCombinations: Vec<Vec<&Letter>> = vec![];
-    let mut combination: Vec<&Letter> = vec![];
-    let mut visited: Vec<Vec<bool>> = vec![vec![false; size as usize]; size as usize];
+    let mut all_combinations: Vec<Vec<&Letter>> = vec![];
+    let combination: Vec<&Letter> = vec![];
+    let mut visited: Vec<Vec<bool>> = vec![vec![false; SIZE as usize]; SIZE as usize];
 
     // start the recursive process from each cell in the grid
     let start = SystemTime::now();
 
-    for row1 in 0..size {
-        for col1 in 0..size {
-            getAllCombinations(
+    for row1 in 0..SIZE {
+        for col1 in 0..SIZE {
+            get_all_combinations(
                 &grid,
                 row1,
                 col1,
                 &mut visited,
                 combination.clone(),
-                &mut allCombinations,
+                &mut all_combinations,
                 4,
             );
         }
@@ -113,7 +117,7 @@ fn main() {
     let end = SystemTime::now();
     let duration = end.duration_since(start).unwrap();
 
-    for (i, row) in allCombinations.iter().enumerate() {
+    for (_, row) in all_combinations.iter().enumerate() {
         let res = row
             .iter()
             .map(|x| String::from(x.key))
@@ -122,5 +126,5 @@ fn main() {
         print!("{}\n", res);
     }
     println!("it took {} seconds", duration.as_secs());
-    println!("ho, {:?}", allCombinations.len());
+    println!("ho, {:?}", all_combinations.len());
 }
