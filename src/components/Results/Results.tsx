460f325f82917841b3bc11a0776bd47c0024e181
Syncxv <aruldevarackal1@gmail.com>
2023-02-06 11:53:19 +1100

    :D

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -26,10 +26,19 @@ export const Results: React.FC<Props> = ({ onNewBoard }) => {
     return (
         <div className="input-wrapper w-full h-full flex flex-col justify-between items-start pt-[7.75rem] px-8">
             <div className="hey w-full">
-                <div className="res">
-                    {tab === -1 ? <div>Loading</div> : results.map(words => {
-                        return words.map((word, i) => <div key={i}>{word}</div>);
-                    })}
+                <div className="res flex flex-col">
+                    <div className="tabs flex flex-wrap">
+                        {tab === -1 ? <div>Loading</div> : results.map((words, i) =>
+                            <div key={i} className="">
+                                <button onClick={() => setTab(i)} className={"bg-slate-300 hover:bg-slate-400 text-gray-900 font-medium py-2 px-4"}>
+                                    {i + 1} Letter{i >= 1 ? "s" : ""}
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                    <div className="word-wrapper flex gap-4 flex-wrap">
+                        {results.length && results[tab].length ? results[tab].map(word => <div>{word}</div>) : <p>No words eh</p>}
+                    </div>
                 </div>
             </div>
 
@@ -37,3 +46,4 @@ export const Results: React.FC<Props> = ({ onNewBoard }) => {
         </div>
     );
 };
+
